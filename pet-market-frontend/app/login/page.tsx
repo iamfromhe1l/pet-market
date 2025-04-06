d8cbd980a06558c91f6922e90f2d6343ac8e0c6a
iamfromhe1l <sgadgsfad@gmail.com>
2025-04-06 21:14:42 +0300

    init base theme & theme layout & base auth pages & prettier + eslint

---
+++ pet-market-frontend/app/login/page.tsx
@@ -0,0 +1,35 @@
+import { PawPrint } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AuthForm } from '@/components/auth-form';
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link href="/" className="flex items-center gap-2">
+            <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-md">
+              <PawPrint className="size-5" />
+            </div>
+            <p className="text-accent-foreground font-bold">PetMarket</p>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-sm">
+            <AuthForm authType="login" />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden overflow-hidden rounded-tl-xl rounded-bl-xl lg:block">
+        <Image
+          src="/login-cat.jpg"
+          className="absolute inset-0 h-full w-full object-cover"
+          alt="auth-cat"
+          quality={100}
+          fill
+        />
+      </div>
+    </div>
+  );
+}
