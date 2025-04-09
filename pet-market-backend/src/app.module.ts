da53a39f44d770cd5f3d7ef515c019570266625a
Kamil <alicesun65@gmail.com>
2025-04-09 19:15:19 +0300

    B2 Squash commit
    Squashed commit of the following:
    
    commit a8bc95247dd1895c199bec5102314a16ee560c8b
    Author: Kamil <alicesun65@gmail.com>
    Date:   Wed Apr 9 19:12:52 2025 +0300
    
        Created users service, dto and constant errors
    
    commit e581e5d71d13af0417a197df150fc17767bba5fe
    Author: Kamil <alicesun65@gmail.com>
    Date:   Wed Apr 9 18:56:57 2025 +0300
    
        Created Users Schema and Collection
    
    commit ae7921b998a73d0350bac8c5da6af894ceddfc9d
    Author: Kamil <alicesun65@gmail.com>
    Date:   Wed Apr 9 18:38:23 2025 +0300
    
        Style fixes after enabling prettierrc
    
    commit 545628df83a0bbb00395316b16379f47e93d2819
    Author: Kamil <alicesun65@gmail.com>
    Date:   Wed Apr 9 18:36:51 2025 +0300
    
        Created mongoConfig and added nest ConfigModule

--- pet-market-backend/src/app.module.ts
+++ pet-market-backend/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { getMongoConfig } from "./db/mongo.config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypegooseModule } from "nestjs-typegoose";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
+        UsersModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
---
+++ pet-market-backend/src/common/constants/errors.ts
@@ -0,0 +1,11 @@
+export enum Errors {
+    USER_NOT_FOUND = "Пользователь с таким email или паролем не найден!",
+    ALREADY_REGISTERED = "Пользователь с таким email уже зарегестрирован!",
+    RT_HASH_NOT_FOUND = "Пользователь вышел из аккаунта!",
+    RT_HASH_INVALID = "Неверный refresh токен",
+    NOT_AUTHENTICATED = "access токен не валидный!",
+    NOT_AUTHORIZED = "Пользователь не авторизован!",
+    CONNECTION_ERROR = "Ошибка подключения к серверу!",
+    SERVER_ERROR = "Ошибка на сервере 404!",
+    UNEXPECTED_ERROR = "Неизвестная ошибка!",
+}
