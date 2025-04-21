29d473817ccd41046985235c8a48b92fbc62ef0b
iamfromhe1l <sgadgsfad@gmail.com>
2025-04-21 23:50:36 +0300

    kennel page get kennel on load & layout

--- pet-market-frontend/app/kennel/[id]/page.tsx
+++ pet-market-frontend/app/kennel/[id]/page.tsx
@@ -2,18 +2,35 @@
 
 import { Header } from '@/components/header';
 import { useKennel } from '@/context/kennel/kennel-context';
-import { use } from 'react';
+import { useRouter } from 'next/navigation';
+import { use, useEffect } from 'react';
+import { toast } from 'sonner';
 
 export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params);
-  const { kennelState } = useKennel();
+  const { kennelState, onGetKennel, loading } = useKennel();
+  const router = useRouter();
+
+  useEffect(() => {
+    const getKennel = async () => {
+      const res = await onGetKennel!(id);
+
+      if (res && res.error) {
+        toast.error(res.error);
+        router.replace('/');
+      }
+    };
+
+    getKennel();
+  }, []);
 
   return (
-    <div>
-      <Header />
-      <div className="grid gap-4">
-        <p>ID: {id}</p>
-      </div>
+    <div className="grid gap-4">
+      <p>ID: {id}</p>
+      <p>Name: {kennelState?.kennel?.name}</p>
+      <p>Description: {kennelState?.kennel?.description}</p>
+      <p>Address: {kennelState?.kennel?.address}</p>
+      <p>Status: {kennelState?.kennel?.status}</p>
     </div>
   );
 }
