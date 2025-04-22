e0778c3e179c7f340a1ae810a21e5b219e2941f7
iamfromhe1l <sgadgsfad@gmail.com>
2025-04-22 12:02:54 +0300

    become kennel test button & unauthorized error toast

--- pet-market-frontend/components/not-public-layout.tsx
+++ pet-market-frontend/components/not-public-layout.tsx
@@ -4,12 +4,14 @@ import { useAuth } from '@/context/auth/auth-context';
 import { redirect } from 'next/navigation';
 import React, { PropsWithChildren, useEffect } from 'react';
 import { PageLoading } from './page-loading';
+import { toast } from 'sonner';
 
 export const NotPublicLayout: React.FC<PropsWithChildren> = ({ children }) => {
   const { authState, loading } = useAuth();
 
   useEffect(() => {
     if (!loading && !authState?.authenticated) {
+      toast.error('Вы не авторизованы!');
       redirect('/login');
     }
   }, [authState, loading]);
